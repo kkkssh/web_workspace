0cfc90ac52b6bd7d4027450c2faec7d2c7fbe3b6
kkkssh <sanghee660@gmail.com>
2023-08-21 11:25:39 +0900

    #8 로그인_회원정보 dao 구현

---
+++ jspBoard/src/main/java/org/iclass/dao/BookUserDao.java
@@ -0,0 +1,19 @@
+package org.iclass.dao;
+
+import java.util.Map;
+
+import org.apache.ibatis.session.SqlSession;
+import org.iclass.dto.BookUser;
+
+import mybatis.SqlSessionBean;
+
+
+public class BookUserDao {		
+	
+	public BookUser login(Map<String,String> map)  {
+		SqlSession mapperSession = SqlSessionBean.getSession();
+		BookUser vo = mapperSession.selectOne("bookuser.login",map);
+		mapperSession.close();
+		return vo;
+	}
+}
